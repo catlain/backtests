a5612a03a9c53f1b210ca5f53688752ea92bfb1c
catlain <catlain@vip.qq.com>
2019-04-30 20:55:44 +0800

    初步跑通回测

---
+++ highlow.R
@@ -0,0 +1,81 @@
+highlow <- function(x, period = "week", n = 60) {
+  
+  # 将日期取出换算成年月日
+  df <- as_tibble(x) %>%
+    mutate(date = as_date(index(x)), 
+           year =  year(date), 
+           month = month(date), 
+           week = month(date), 
+           day = day(date))
+  
+  # 重命名各列判断 adjusted 与 close 的关系
+  
+  get_col <- function(df, col = "Open"){
+    names(df)[str_detect(names(df), col)]
+  }
+  
+  df <- df %>%
+    rename(
+      # open = get_col(., "Open"),
+      # high = get_col(., "High"),
+      # low = get_col(., "Low"),
+      # close = get_col(., "Close"),
+      # volume = get_col(., "Volume"),
+      adjusted = get_col(., "Adjusted")) %>%
+    select(adjusted:day)
+  
+  # 按年月计算最后一个交易日的 adjusted
+  period_df <- group_by(df, year, !! as.symbol(period)) %>% # 之前的方法 enquo() + !! 的方法可能不适用了
+    arrange(day) %>%
+    summarise(adjusted = last(adjusted), 
+              date = last(date)) 
+  
+  # 计算月度收益率及收益率差异,相当于计算1阶2阶导数
+  period_df <- ungroup(period_df) %>%
+    mutate(profit = adjusted - lag(adjusted),
+           diff = profit - lag(profit))
+  
+  # 高点为本月的 diff > 0,次月的diff < 0,低点反之
+  period_df <- mutate(period_df, 
+                    if_high = if_else(profit > 0 & lead(profit) < 0, TRUE, FALSE), 
+                    if_low = if_else(profit < 0 & lead(profit) > 0, TRUE, FALSE))
+  
+  # 给高点排序
+  period_high <- filter(period_df, if_high) %>%
+    arrange(desc(adjusted)) %>%
+    mutate(rank = row_number())
+  
+  # 给底点排序
+  period_low <- filter(period_df, if_low) %>%
+    arrange(adjusted) %>%
+    mutate(rank = row_number())
+  
+  # 求高低点前 days 天的日期
+  period_high <- period_high %>%
+    mutate(date_last_period = date - days(n))
+  # date_last_period = `year<-`(date, year(date) - 1), # 求1年前的日期
+  
+  # 时段内的高点意味着本期高点比时段内其他高点高
+  period_high$high_last_period <- seq_len(nrow(period_high)) %>%
+    map_dbl(~ max(filter(period_high, date <= period_high$date[.x], date > period_high$date_last_period[.x])$adjusted))
+  period_high <- filter(period_high, if_else(adjusted >= high_last_period, TRUE, FALSE)) %>% 
+    arrange(desc(date))
+  
+  period_low <- period_low %>%
+    mutate(date_last_period = date - days(n))
+  period_low$low_last_period <- seq_len(nrow(period_low)) %>%
+    map_dbl(~ min(filter(period_low, date <= period_low$date[.x], date > period_low$date_last_period[.x])$adjusted))
+  period_low <- filter(period_low, if_else(adjusted <= low_last_period, TRUE, FALSE)) %>%
+    arrange(desc(date))
+  
+  
+  df$high_last_period <- seq_len(nrow(df)) %>%
+    map_dbl(~ first(filter(period_high, date <= df$date[.x])$adjusted))
+  
+  df$low_last_period <- seq_len(nrow(df)) %>%
+    map_dbl(~ first(filter(period_low, date <= df$date[.x])$adjusted))
+  
+  select(df, adjusted, high_last_period, low_last_period) %>%
+    zoo(., df$date) %>% # 不用.是因为需要 date 列保留
+    xts()
+}
---
+++ highlow_backtest.R
@@ -0,0 +1,188 @@
+require(quantstrat)
+require(IKTrading)
+require(rlang)
+require(dplyr)
+require(ggplot2)
+require(lubridate)
+require(purrr)
+# require(PerformanceAnalytics)
+# require(FinancialInstrument)
+# suppressWarnings(rm("order_book.highlow", pos = .strategy))
+# suppressWarnings(rm("account.highlow", "portfolio.highlow", pos = .blotter))
+# suppressWarnings(rm("account_st", "portfolio_st", "stock_str"))
+
+rm(list = ls(.blotter), envir = .blotter)
+rm(list = ls(.strategy), envir = .strategy)
+source("highlow.R")
+
+stock_str <- "601166.SS" # 需要回测的股票
+init_date <- "2015-01-01"
+# 从雅虎取得股票数据
+getSymbols(stock_str, from = init_date, index.class=c("POSIXt","POSIXct"))
+# stock_df <- na.omit(adjustOHLC(get(stock_str), use.Adjusted = TRUE))
+assign(stock_str, na.omit(adjustOHLC(get(stock_str), use.Adjusted = TRUE)))
+
+# 设定货币为人民币
+# rm_instruments()
+# ls_instruments
+
+currency("CNY")
+Sys.setenv(TZ = "UTC")
+stock(stock_str, currency = "CNY", multiplier = 1)
+
+# 设定资金量及最大头寸
+trade_size <- 100000
+init_eq <- trade_size * length(stock_str)
+pct_atr <- 0.2
+n_atr <- 10
+n_ema <- 5
+period_highlow <- "week"
+n_high <- 60
+n_low <- 40
+buyCost <- 0.001425
+sellCost <- 0.004425
+
+# 手续费
+buyFee <- function(TxnQty, TxnPrice, Symbol, ...)
+{
+  abs(TxnQty) * TxnPrice * -buyCost
+}
+
+sellFee <- function(TxnQty, TxnPrice, Symbol, ...)
+{
+  abs(TxnQty) * TxnPrice * -sellCost
+}
+
+# 初始化
+strategy_st <- portfolio_st <- account_st <- strat_st <-  "highlow"
+rm.strat(portfolio_st)
+rm.strat(strategy_st)
+initPortf(portfolio_st, symbols = stock_str, initDate = init_date, currency = "CNY")
+initAcct(account_st, portfolios = portfolio_st, init_eq = init_eq, initDate = init_date, currency = "CNY")
+initOrders(portfolio = portfolio_st, initDate = init_date)
+
+# 设定策略
+strategy(strategy_st, store = TRUE)
+
+# 指标
+# add.indicator(strategy_st, name = "ATR",
+#               arguments = list(HLC = quote(HLC(mktdata)), n = n_atr),
+#               label = "atr")
+
+add.indicator(strategy_st, name = "EMA",
+              arguments = list(x = quote(Cl(mktdata)), n = n_ema),
+              label = "EMAL")
+
+add.indicator(strategy_st, name = "highlow", 
+              arguments = list(x = quote(Ad(mktdata)), period = period_highlow, n = n_high), 
+              label = "High")
+
+add.indicator(strategy_st, name = "highlow", 
+              arguments = list(x = quote(Ad(mktdata)), period = period_highlow, n = n_low), 
+              label = "Low")
+
+
+# 信号
+
+add.signal(strategy_st, name = "sigFormula",
+           arguments = list(data = quote(mktdata),
+                            formula = "EMA.EMAL > high_last_period.High"), 
+           label = "BuySignal"
+)
+
+add.signal(strategy_st, name = "sigFormula",
+           arguments = list(data = quote(mktdata),
+                            formula = "EMA.EMAL < low_last_period.Low"), 
+           label = "SellSignal"
+)
+
+# add.signal(strategy = strategy_st, name = "sigCrossover", 
+#            arguments = list(columns = c("EMA.EMAL", "high_last_period.HighLow"), 
+#                             relationship = "gt"), 
+#            label = "BuySignal")
+# 
+# add.signal(strategy = strategy_st, name = "sigCrossover", 
+#            arguments = list(columns = c("EMA.EMAL", "low_last_period.HighLow"), 
+#                             relationship = "lt"),
+#            label = "SellSignal")
+
+# 进出场
+add.rule(strategy_st, name = "ruleSignal",
+         arguments = list(sigcol = "BuySignal",
+                          sigval = TRUE,
+                          ordertype = "market",
+                          orderside = "long",
+                          replace = TRUE, #  replace = TRUE may choose just one rule
+                          prefer = "Open", #  tomorrow because today has closed
+                          osFUN = osMaxDollar, #  order size function
+                          tradeSize = trade_size,
+                          maxSize = trade_size,
+                          pct_atr = pct_atr,
+                          maxpct_atr = pct_atr, #  set an upper limit of orders
+                          atrMod = "",
+                          TxnFees = "buyFee"), #  atrx above
+         type = "enter", path.dep = TRUE, label = "EnterRule")
+
+
+add.rule(strategy_st, name = "ruleSignal",
+         arguments = list(sigcol = "SellSignal",
+                          sigval = TRUE,
+                          orderqty = "all", #  order quantity, in all and out all
+                          ordertype = "market",
+                          orderside = "long",
+                          replace = FALSE,
+                          prefer = "Open",
+                          TxnFees = "sellFee"),
+         type = "exit", path.dep = TRUE, label = "ExitRule")
+
+
+
+
+# add.rule(strategy = strategy_st, name = 'ruleSignal', 
+#          arguments = list(sigcol = "BuySignal", 
+#                           sigval = TRUE, 
+#                           ordertype = 'market', 
+#                           orderside = 'long', 
+#                           orderqty = "all",
+#                           prefer =  "Close"), 
+#          type = 'enter', label = "EnterRule", enabled = T)
+
+# add.rule(strategy = strategy_st, name = 'ruleSignal', 
+#          arguments = list(sigcol = "SellSignal", 
+#                           sigval = TRUE, 
+#                           orderqty = 'all', 
+#                           ordertype = 'market', 
+#                           orderside = 'long', prefer = "Close"), 
+#          type = 'exit', label = "ExitRule", enabled = T)
+
+out <- applyStrategy(strategy = strategy_st, 
+              portfolios = portfolio_st, symbols = stock_str)
+
+# 更新账户资料和权益金额
+updatePortf(Portfolio = portfolio_st)
+tradeDetails <- getPortfolio(portfolio_st)
+posPL <- tradeDetails$symbols$tsmc$posPL
+dateRange <- time(tradeDetails$summary)[-1]
+updateAcct(portfolio_st, dateRange)
+updateEndEq(account_st)
+
+# TODO 检查没有成交的原因
+trend1_book <- getOrderBook(portfolio = strategy_st)
+# trend1_book
+
+
+# 統計資料
+tStats <- tradeStats(Portfolios = portfolio_st, use = "trades", inclZeroDays = FALSE)
+tStats[, 4:ncol(tStats)] <- round(tStats[, 4:ncol(tStats)], 2)
+(aggPF <- sum(tStats$Gross.Profits)/-sum(tStats$Gross.Losses))
+(aggCorrect <- mean(tStats$Percent.Positive))
+(numTrades <- sum(tStats$Num.Trades))
+(meanAvgwLR <- mean(tStats$Avg.WinLoss.Ratio[tStats$Avg.WinLoss.Ratio < Inf], na.rm = TRUE))
+
+# 畫k線圖、進出場flag、持有部位和累積報酬
+myTheme <- chart_theme()
+myTheme$col$dn.col<-'lightgray'
+myTheme$col$dn.border <- 'lightgray'
+myTheme$col$up.border <- 'lightgray'
+
+chart.Posn(Portfolio = portfolio_st, Symbol = stock_str, theme = myTheme)
